88ad3badb5d80578b0e4de3bcf0ab080c5e9b313
Noah Murad <noahmurad@hotmail.com>
2016-04-03 16:52:56 -0400

    proper resizing of cards

--- js/app.js
+++ js/app.js
@@ -1,17 +1,25 @@
 $(document).ready(function() {
 	//load navbar code, then callback when it's done
-	$('#top_navbar').load("top_navbar.html", load_page());
+	$('#top_navbar').load("top_navbar.html");
 	$('#leftnav_wrapper').load("left_navbar.html");
 
 	set_center_height();
+    set_card_width();
 });
 
-function load_page() {
+//make spaces around card smaller to fit in bulletin board on index.html
+function set_card_width() {
 	var pathname 	= location.pathname;
 	var last_slash 	= pathname.lastIndexOf("/");
 	var curr_page 	= pathname.substring(last_slash+1);
-
 	var wss = "Western Student Centre";
+
+    var flex_value = "0 0 5%"
+
+    if(curr_page.localeCompare("index.html") === 0) {
+        $('#left_space').css({'flex': flex_value});
+        $('#right_space').css({'flex': flex_value});
+    }
 }
 
 //when the window is resized, call this function
